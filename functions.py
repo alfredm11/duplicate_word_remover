a3abcf14deac510b38a421100375233a46188080
alfredm11 <alfredmalaza11@gmail.com>
2023-11-08 19:43:13 +0800

    Adding summary

--- functions.py
+++ functions.py
@@ -2,26 +2,46 @@ import re
 
 
 def remove_duplicate(text):
+    '''
+        Remove duplicated word.
+        Example: many many, for for, has has, etc.
+    '''
 
     pattern = re.compile(r"\b([a-zA-Z]+)\s?\1\b", flags=re.IGNORECASE)
     matches = pattern.finditer(text)
 
     sub_words = pattern.sub(r'\1', text)
-    return remove_duplicate_comma(remove_duplicate_apostrophe(sub_words))
+    return remove_duplicate_1comma(remove_duplicate_apostrophe(sub_words))
 
 
 def remove_duplicate_apostrophe(text):
+    '''
+        Remove duplicated contracted word regardless of case.
+        Example: It's it's, He's he's, etc.
 
+    '''
     pattern = re.compile(r'\b([a-zA-Z]+\'\w)\s?\1\b', flags=re.IGNORECASE)
     matches = pattern.finditer(text)
 
-    sub_words = pattern.sub(r'\1', text)
+
+def remove_duplicate_2_comma(text):
+    '''
+        Removes duplicated words with comma.
+        Example: (in, in, in), (very, very, very)
+    '''
+    pattern = re.compile(r'(\w+\,)\s\1\s(\w+)', flags=re.IGNORECASE)
+    matches = pattern.finditer(text)
+
+    sub_words = pattern.sub(r'\2', text)
     return sub_words
 
 
-def remove_duplicate_comma(text):
-
-    pattern = re.compile(r'(\w+\,)\s\1\s(\w+)', flags=re.IGNORECASE)
+def remove_duplicate_1comma(text):
+    '''
+        Removes duplicated words with comma.
+        Example: (in, in), (very, very)
+    '''
+    pattern = re.compile(r'(\w+\,)\s(\w+)', flags=re.IGNORECASE)
     matches = pattern.finditer(text)
 
     sub_words = pattern.sub(r'\2', text)
